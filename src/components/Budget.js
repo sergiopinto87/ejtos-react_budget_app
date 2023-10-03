e8b80fe3bda2dc047c5579f3b89bce82a9a92289
Sergio Pinto <serrique@icloud.com>
2023-10-03 14:57:16 -0400

    Fixed budget update based on user input
    Signed-off-by: Sergio Pinto <serrique@icloud.com>

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -24,7 +24,7 @@ const Budget = () => {
     return (
 <div className='alert alert-secondary'>
 <span>Budget: £</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<input type="number" step="10" value={budget} onChange={handleBudgetChange}></input>
 
 </div>
     );
